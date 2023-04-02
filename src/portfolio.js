f06ee8dd9b4d13578d19b304f80bbe83b0d736dc
Sumedh Vichare <sumedhuvichare@gmail.com>
2023-04-02 10:22:45 +0530

    contact details added

--- src/portfolio.js
+++ src/portfolio.js
@@ -403,8 +403,8 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+91 9820817092",
+  email_address: "sumedhuvichare@gmail.com"
 };
 
 // Twitter Section
